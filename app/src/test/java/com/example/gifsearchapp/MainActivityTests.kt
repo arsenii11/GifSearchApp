701307004c7698324964b63e127e083419508171
Arsenii Dragunkin <57676564+arsenii11@users.noreply.github.com>
2023-01-30 02:04:27 +0200

    Tests dependencies

---
+++ app/src/test/java/com/example/gifsearchapp/MainActivityTests.kt
@@ -0,0 +1,17 @@
+package com.example.gifsearchapp
+
+import androidx.lifecycle.LiveData
+import com.example.gifsearchapp.data.DataObject
+import com.example.gifsearchapp.data.DataResult
+import com.example.gifsearchapp.presentation.main.MainViewModel
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import java.util.*
+
+
+class MainActivityTests {
+
+    }
