ec4a6bc4e38308b105448c653c31d269b9e9cbfd
Arsenii Dragunkin <57676564+arsenii11@users.noreply.github.com>
2023-01-31 05:40:03 +0200

    GiphyLoadListAdapter.kt

---
+++ app/src/main/java/com/example/gifsearchapp/presentation/main/GiphyLoadListAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.gifsearchapp.presentation.main
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.paging.LoadState
+import androidx.paging.LoadStateAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class GiphyLoadListAdapter(private val retry: () -> Unit)/*: LoadStateAdapter<GiphyLoadListAdapter.PassengerLoadStateViewHolder>() */{
+    /*inner class PassengerLoadStateViewHolder(
+        private val binding: ,
+        private val retry: () -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(loadState: LoadState) {
+            if (loadState is LoadState.Error) {
+                binding.textViewError.text = loadState.error.localizedMessage
+            }
+
+            binding.progressbar.visible(loadState is LoadState.Loading)
+            binding.buttonRetry.visible(loadState is LoadState.Error)
+            binding.textViewError.visible(loadState is LoadState.Error)
+            binding.buttonRetry.setOnClickListener {
+                retry()
+            }
+
+            binding.progressbar.visibility = View.VISIBLE
+        }
+    }
+
+    override fun onBindViewHolder(holder: PassengerLoadStateViewHolder, loadState: LoadState) {
+        holder.bind(loadState)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        loadState: LoadState
+    ) = PassengerLoadStateViewHolder(
+        ItemLoadingStateBinding.inflate(LayoutInflater.from(parent.context), parent, false),
+        retry
+    )
+*/
+}
